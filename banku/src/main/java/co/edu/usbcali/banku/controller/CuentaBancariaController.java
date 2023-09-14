c9f982cdef55a9882651b44ff5b479f3ad486439
Laura <lauracast142@gmail.com>
2023-09-14 15:44:17 -0500

    creación de todos los controllers

---
+++ banku/src/main/java/co/edu/usbcali/banku/controller/CuentaBancariaController.java
@@ -0,0 +1,12 @@
+package co.edu.usbcali.banku.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cuentaBancaria")
+public class CuentaBancariaController {
+
+}
